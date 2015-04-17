77e17174c3c6a6b04cc34a6443c69eb07e5b49cc
Ed Robinson <ed.robinson@reevoo.com>
2015-04-17 13:08:21 +0100

    Refactor for MultiThreaded operation

--- sitemap_check.rb
+++ sitemap_check.rb
@@ -1,103 +1,157 @@
 require 'nokogiri'
 require 'httpclient'
 require 'colorize'
+require 'thread'
 
-class Sitemap
-  def initialize(url)
-    self.url = url
-    setup_doc
-    self.checked = 0
+class SitemapCheck
+
+  def self.check
+    $stdout.sync = true
+    new.check
   end
 
-  attr_accessor :doc, :url, :checked
-
-  def sitemaps
-    maps.map do |sitemap|
-      map = Sitemap.new(sitemap.loc.text)
-      [map] + map.sitemaps
-    end.flatten.uniq(&:url)
+  def initialize
+    puts "Expanding Sitemaps from #{ENV['CHECK_URL']}"
+    self.sitemaps = Sitemap.new(ENV['CHECK_URL']).sitemaps
   end
 
-  def missing_pages
-    @_misssing ||= page_urls.map do |page_url|
-      self.checked += 1
-      unless page_exists?(page_url)
-        puts "  missing: #{page_url}".red
-        page_url
-      end
-    end.compact
+  def check
+    check_indexes
+    check_pages
+    exit exit_code
   end
 
-  def exists? # rubocop:disable Style/TrivialAccessors
-    @ok
-  end
+  protected
+
+  attr_accessor :sitemaps, :exit_code
 
   private
 
-  def page_exists?(page_url)
-    tries = 0
-    http = HTTPClient.new
-    http.get(page_url, follow_redirect: true).ok?
-  rescue SocketError, HTTPClient::ConnectTimeoutError
-    tries += 1
-    if tries < 5
-      sleep 1
-      retry
-    else
-      false
+  def check_indexes
+    sitemaps.reject(&:exists?).each do |sitemap|
+      puts "#{sitemap.url} does not exist".red.bold
+      self.exit_code = 1
     end
-  rescue HTTPClient::BadResponseError
-    false
+    puts ''
   end
 
-  def setup_doc
-    http = HTTPClient.new
-    response = http.get(url, follow_redirect: true)
-    return unless (@ok = response.ok?)
-    self.doc = Nokogiri::Slop(response.body)
-    doc.remove_namespaces!
-  rescue HTTPClient::BadResponseError
-    @ok = false
-  end
-
-  def page_urls
-    doc.urlset.url.map { |url| url.loc.text }
-  rescue NoMethodError
-    []
-  end
-
-  def maps
-    doc.sitemapindex.sitemap
-  rescue NoMethodError
-    []
-  end
-end
-
-$stdout.sync = true
-exit_code = 0
-puts "Expanding Sitemaps from #{ENV['CHECK_URL']}"
-sitemaps = Sitemap.new(ENV['CHECK_URL']).sitemaps
-
-sitemaps.reject(&:exists?).each do |sitemap|
-  puts "#{sitemap.url} does not exist".red.bold
-  exit_code = 1
-end
-
-puts ''
-
-sitemaps.select(&:exists?).each do |sitemap|
-  puts "Checking #{sitemap.url}"
-  if sitemap.missing_pages.any?
-    exit_code = 1
-    puts "checked #{sitemap.checked} pages and #{sitemap.missing_pages.count} were missing".red.bold
-  else
-    if sitemap.checked > 0
-      puts "checked #{sitemap.checked} pages and everything was ok".green.bold
-    else
-      puts "this sitemap did not contain any pages".green
+  def check_pages
+    sitemaps.select(&:exists?).each do |sitemap|
+      puts "Checking #{sitemap.url}"
+      if sitemap.missing_pages.any?
+        self.exit_code = 1
+        puts "checked #{sitemap.checked} pages and #{sitemap.missing_pages.count} were missing".red.bold
+      else
+        if sitemap.checked > 0
+          puts "checked #{sitemap.checked} pages and everything was ok".green.bold
+        else
+          puts 'this sitemap did not contain any pages'.green
+        end
+      end
+      puts ''
     end
   end
-  puts ''
+
+  class Sitemap
+    def initialize(url)
+      self.url = url
+      self.checked = 0
+      setup_doc
+    end
+
+    attr_accessor :doc, :url, :checked
+
+    def sitemaps
+      maps.map do |sitemap|
+        map = Sitemap.new(sitemap.loc.text)
+        [self, map] + map.sitemaps
+      end.flatten.uniq(&:url)
+    end
+
+    def missing_pages
+      @_misssing ||= find_missing_pages
+    end
+
+    def exists? # rubocop:disable Style/TrivialAccessors
+      @ok
+    end
+
+    private
+
+    def http
+      @_http ||= HTTPClient.new
+    end
+
+    def concurency
+      ENV.fetch('CONCURENCY', 10)
+    end
+
+    def find_missing_pages
+      q = Queue.new
+      mutex = Mutex.new
+      pages.each { |page| q.push page }
+      concurency.times.map do
+        Thread.new do
+          begin
+            while page = q.pop(true)
+              unless page.exists?
+                puts "  missing: #{page.url}".red
+                page
+              end
+              mutex.synchronize { self.checked +=1 }
+            end
+          rescue ThreadError
+          end
+        end
+      end.each(&:join)
+      pages.reject(&:exists?)
+    end
+
+    def setup_doc
+      response = http.get(url, follow_redirect: true)
+      return unless (@ok = response.ok?)
+      self.doc = Nokogiri::Slop(response.body)
+      doc.remove_namespaces!
+    rescue HTTPClient::BadResponseError
+      @ok = false
+    end
+
+    def pages
+      doc.urlset.url.map { |url| Page.new(url.loc.text, http) }
+    rescue NoMethodError
+      []
+    end
+
+    def maps
+      doc.sitemapindex.sitemap
+    rescue NoMethodError
+      []
+    end
+  end
+
+  class Page
+    def initialize(url, client = HTTPClient.new)
+      self.url = url
+      self.http = http
+    end
+
+    attr_accessor :url, :http
+
+    def exists?
+      tries = 0
+      @_exists ||= http.head(url, follow_redirect: true).ok?
+    rescue SocketError, HTTPClient::ConnectTimeoutError
+      tries += 1
+      if tries < 5
+        sleep 1
+        retry
+      else
+        @_exists = false
+      end
+    rescue HTTPClient::BadResponseError
+      @_exists = false
+    end
+  end
 end
 
-exit exit_code
+SitemapCheck.check
